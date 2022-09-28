ea08cbdabd543742bd88de776fc6daefb76d6e7c
zoujunqi <zou5@ualberta.ca>
2022-09-28 13:30:50 -0400

    update uni column name

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -27,7 +27,7 @@ class ColumbiaStudentResource:
     @staticmethod
     def get_by_key(key):
 
-        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
+        sql = "SELECT * FROM E6156_hw.columbia_student where uni=%s";
         conn = ColumbiaStudentResource._get_connection()
         cur = conn.cursor()
         res = cur.execute(sql, args=key)
